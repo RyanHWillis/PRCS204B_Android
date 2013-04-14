ff994000e7a8b436c8133fc50bc145fc24da1729
Reece Hanham <rhanham93@gmail.com>
2013-04-14 13:58:18 +0100

    My Bookings ListView items now have a image icon.

--- src/com/prcs204b/mobile/app/MyBookingsFragment.java
+++ src/com/prcs204b/mobile/app/MyBookingsFragment.java
@@ -4,26 +4,69 @@ import com.prcs204b.mobile.model.Event;
 import com.prcs204b.mobile.model.Booking;
 
 import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MyBookingsFragment extends Fragment {
 	
+	static class BookingHolder { 
+		ImageView bookingIcon;
+		TextView  bookingText;
+	}
+	
 	private class MyBookingsAdapter extends ArrayAdapter<Booking>
 	{
-		public MyBookingsAdapter(Context context, int textViewResourceId) {
-			super(context, textViewResourceId);
+		private Context context;
+		private int		layoutResourceId;
+		
+		public MyBookingsAdapter(Context context, int layoutResourceId) {
+			super(context, layoutResourceId);
+			
+			this.context = context;
+			this.layoutResourceId = layoutResourceId;
 		}
 		
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			return super.getView(position, convertView, parent);
-		}		
+			
+			
+			View row = convertView;
+			BookingHolder holder;
+			
+			if(row == null) {
+				
+				// Get the layout inflater.
+				LayoutInflater inflater = 
+						((Activity)context).getLayoutInflater();
+				
+				row = inflater.inflate(layoutResourceId, parent, false);
+			
+				holder = new BookingHolder();
+				holder.bookingIcon = (ImageView)row.findViewById(R.id.booked_event_display);
+				holder.bookingText = (TextView)row.findViewById(R.id.booked_event_title_qty);
+				
+				row.setTag(holder);
+			
+			} else { 				
+				holder = (BookingHolder)row.getTag();
+			}
+			
+			Booking booking = getItem(position);
+			holder.bookingText.setText(booking.toString());
+			holder.bookingIcon.setImageResource(android.R.drawable.btn_star);
+			
+			return row;
+		}
+		
+		
 	}
 	
 	
@@ -47,7 +90,7 @@ public class MyBookingsFragment extends Fragment {
 		Event aliceInWonderlandEvent = new Event("Alice in Wonderland");
 		Event warOfTheWorldsEvent = new Event("War of the Worlds");
 		
-		mBookingsAdapter = new MyBookingsAdapter(getActivity(), android.R.layout.simple_list_item_1);
+		mBookingsAdapter = new MyBookingsAdapter(getActivity(), R.layout.adapter_bookings_listitem);
 		for(int i = 0; i < 5; i++) {
 			mBookingsAdapter.add( new Booking(warOfTheWorldsEvent, 2+i));
 			mBookingsAdapter.add( new Booking(aliceInWonderlandEvent, 3+i));
